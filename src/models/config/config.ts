7781a52057ba220715c9e97919f4abf257ec51d1
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2021-08-27 15:46:58 +0200

    Set public server url

--- src/models/config/config.ts
+++ src/models/config/config.ts
@@ -1,5 +1,5 @@
 export default class ConfigModel
 {
-	public static ServerURL = "https://192.168.0.3/socket.io/"
+	public static ServerURL = "https://spaces.millicast.com/socket.io/"
 	public static ServerOptions = { transports: ["websocket"] }
 }
